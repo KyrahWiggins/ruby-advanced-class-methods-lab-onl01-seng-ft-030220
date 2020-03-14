213b0670a16ddd0f5732a956f1268168a6262de1
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-14 02:43:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,13 @@
-self.class.all << self
-  
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
 
-
-  def self.create
-    song = self.new
-    self.all << song
-    song.save
-    song
+  def self.all
+    @@all
   end
 
-  def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    #self.all << name
+  def save
+    self.class.all << self
   end
 
-  # def self.create(name)
-  #   person = self.new
-  #   person.name = name
-  #   @@all << person
-  # end
-end	
+end
